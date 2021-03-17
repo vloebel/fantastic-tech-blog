04b410306ab92ac42d68d9f3e78bc53b235fce02
Vicky Loebel <vickyloebel@gmail.com>
2021-03-17 11:16:31 -0700

    added post-text and post-disply to css to control blog post text size. needs post body edit functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../../config/connection');
 const { Post, User, Comment, UserPost } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
@@ -75,7 +75,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_txt: 'https://taskmaster.com/press', user_id: 1}
+  // expects {title: 'Taskmaster goes public!', post_txt: 'a bunch of interesting stuff, user_id: 1}
   Post.create({
     title: req.body.title,
     post_txt: req.body.post_txt,
