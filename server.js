36654a0104ada345c8536804e8f9132c708e5aa0
Vicky Loebel <vickyloebel@gmail.com>
2021-03-18 10:43:29 -0700

    added session timeout & moved sesson var to .env file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+require('dotenv').config();
+const SESS_SECRET = process.env.SESS_SECRET;
+const SESS_TMEOUT = process.env.SESS_TMEOUT
+;
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,14 +14,19 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: SESS_SECRET,
+  cookie: {
+    maxAge: SESS_TMEOUT
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
+console.log(`secret: ${SESS_SECRET}`);
+console.log(`timeout: ${SESS_TMEOUT}`);
+
 
 app.use(session(sess));
 
